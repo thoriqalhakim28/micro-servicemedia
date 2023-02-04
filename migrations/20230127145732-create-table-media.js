d196bd67b4f5be3a27b227d3b837881aaf1595c2
Thoriq Al Hakim <tah280103@gmail.com>
2023-02-04 09:26:43 +0700

    first commit

---
+++ migrations/20230127145732-create-table-media.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const { sequelize } = require('../models');
+
+/** @type {import('sequelize-cli').Migration} */
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('media', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE, 
+        allowNull: false
+      }
+    }); 
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('media');
+  }
+};
